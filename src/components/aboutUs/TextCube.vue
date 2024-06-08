a26e28e458cee2a8c9783a85f414dd44986b1296
Zdadua <2814126219@qq.com>
2024-06-09 00:54:17 +0800

    新建textCube组件(未完成)
    该组件用于包裹标题和描述，提供一个Options即可修改颜色等，未完成

---
+++ src/components/aboutUs/TextCube.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+  // 获取传给该组件的参数
+  import {onMounted, ref} from "vue";
+
+  const props = defineProps({
+    title: String,
+    text: String,
+  })
+
+  let cube = ref(null);
+
+  onMounted(() => {
+    if(cube.value) {
+      const rect = cube.value.getBoundingClientRect();
+    }
+  })
+
+</script>
+
+<template>
+  <!-- 不知道会不会冲突 -->
+  <div ref="cube" id="cube-container">
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
