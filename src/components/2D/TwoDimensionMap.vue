42e6ab32b6243bf1ee2f6381aa7314e5fd3c3f5e
Zdadua <2814126219@qq.com>
2024-06-21 13:37:40 +0800

    添加了Eslint和Stylelint配置

--- src/components/2D/TwoDimensionMap.vue
+++ src/components/2D/TwoDimensionMap.vue
@@ -31,64 +31,6 @@ onMounted(() => {
 
 <style scoped>
 
-  .see-sight {
-    width: 100vw;
-    height: 100vh;
-    background-color: white;
-  }
-
-  #two-d-map-container {
-    position: relative;
-  }
-
-  #map-container {
-    width: 100%;
-    height: 100%;
-  }
-
-  #info-container {
-    box-sizing: border-box;
-    padding: 5px;
-    width: 200px;
-    height: 100px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: absolute;
-    bottom: 10px;
-    left: -50px;
-    background-color: white;
-    border-radius: 10px;
-
-    #info-text {
-      width: 100%;
-      height: 30px;
-      line-height: 30px;
-      font-size: 1em;
-      margin-bottom: 10px;
-    }
-
-    #lon-lat {
-      max-width: 190px;
-      height: 30px;
-      align-content: center;
-      line-height: 30px;
-      font-size: .8em;
-      background-color: #e4e4e4;
-    }
-
-    &:after {
-      content: '';
-      position: absolute;
-      bottom: -10px;
-      left: 40px;
-      width: 0;
-      height: 0;
-      border-style: solid;
-      border-width: 10px 10px 0 10px;
-      border-color: #ffffff transparent transparent transparent;
-    }
-
-  }
+@import "../../styles/map/twoDimensionMap.css";
 
 </style>
